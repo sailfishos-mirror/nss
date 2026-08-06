dc51b81292977ace436f287cd531967332e977fe
John Schanck <jschanck@mozilla.com>
2026-08-06 21:45:13 +0000

    Bug 2061107 - restore pkcs12.h for source compatibility. r=nss-reviewers,keeler
    Differential Revision: https://phabricator.services.mozilla.com/D316884

--- lib/pkcs12/exports.gyp
+++ lib/pkcs12/exports.gyp
@@ -15,6 +15,7 @@
             'p12.h',
             'p12plcy.h',
             'p12t.h',
+            'pkcs12.h',
             'pkcs12t.h'
           ],
           'destination': '<(nss_public_dist_dir)/<(module)'
---
+++ lib/pkcs12/pkcs12.h
@@ -0,0 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef _PKCS12_H_
+#define _PKCS12_H_
+
+#include "pkcs12t.h"
+#include "p12.h"
+
+#endif
