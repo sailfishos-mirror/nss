03892e60aee8c0c686bd09080cc6d43f029351b6
John Schanck <jschanck@mozilla.com>
2026-09-14 18:19:53 +0000

    Bug 2061391 - perform session object removals under slot lock. r=nss-reviewers,keeler
    Differential Revision: https://phabricator.services.mozilla.com/D318563

--- lib/softoken/pkcs11.c
+++ lib/softoken/pkcs11.c
@@ -4629,11 +4629,24 @@ NSC_InitToken(CK_SLOT_ID slotID, CK_CHAR_PTR pPin,
              * because we know that we are freeing all the sessions, we can
              * do more efficient processing */
             if (object) {
+                SFTKSessionObject *so = sftk_narrowToSessionObject(object);
+
                 slot->sessObjHashTable[i] = object->next;
 
                 if (object->next)
                     object->next->prev = NULL;
                 object->next = object->prev = NULL;
+                /* Keep the two queues in agreement: sftk_ClearSession()
+                 * relies on an object being on a session's list only while it
+                 * is still in the slot hash. Leaving it linked would let that
+                 * walk touch this object after we drop the queues' reference
+                 * below. */
+                if (so && so->session) {
+                    PORT_Assert(sftkqueue_is_queued(&so->sessionList, 0,
+                                                    so->session->objects, 0));
+                    sftkqueue_delete(&so->sessionList, 0,
+                                     so->session->objects, 0);
+                }
             }
             if (object)
                 sftk_FreeObject(object);
